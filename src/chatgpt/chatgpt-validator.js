61b9a559e47e92798b82e85fb452168a0669aa9c
Guilherme Toshimitsu <guilherme.toshimitsu@gmail.com>
2023-07-24 10:42:27 -0300

    add: validator

---
+++ src/chatgpt/chatgpt-validator.js
@@ -0,0 +1,22 @@
+const { body } = require("express-validator/check");
+
+// body()
+const validateChatGPTBody = () => {
+  return [body("id", "id nao existe").exists()];
+};
+
+module.exports = {
+  validateChatGPTBody,
+};
+// exports.validate = (method) => {
+//   switch (method) {
+//     case 'createUser': {
+//      return [
+//         body('userName', 'userName doesnt exists').exists(),
+//         body('email', 'Invalid email').exists().isEmail(),
+//         body('phone').optional().isInt(),
+//         body('status').optional().isIn(['enabled', 'disabled'])
+//        ]
+//     }
+//   }
+// }
